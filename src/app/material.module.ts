156ad69a48d04031294e8f1a27f7aae17bf4f421
sdmohite <sdmohite@gmail.com>
2019-04-28 19:18:36 +0530

    added navbar and home component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule, MatTabsModule } from '@angular/material';
+
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatBadgeModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule
+
   ],
-  exports: [MatBadgeModule,
-    MatButtonModule]
+  exports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule
+  ]
 })
 export class MaterialModule { }
